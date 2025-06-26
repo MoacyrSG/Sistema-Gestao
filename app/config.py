cb37f21c95b6faf02ec91b3ad0814d39d7bfc2b9
MoacyrSG <moacyr@coutinhoexcursoes.com>
2025-06-25 21:29:28 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'sua_chave_secreta')
-    SQLALCHEMY_DATABASE_URI = "postgresql+pg8000://camila:4512@localhost:5432/coutinho_excursoes"
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
