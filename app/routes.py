9cefabcc2628cd398179d4c0b38afcf260154c74
MoacyrSG <moacyr@coutinhoexcursoes.com>
2025-07-16 22:28:15 -0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1486,18 +1486,18 @@ def gerar_voucher(id):
     c.setFont("Helvetica", 9)
     c.drawString(50, y, f"Número do {reserva.tipo_reserva}:")
     c.setFont("Helvetica-Bold", 9)
-    c.drawString(150, y, reserva.id_reserva if reserva.id_reserva else "")
+    c.drawString(120, y, reserva.id_reserva if reserva.id_reserva else "")
     
     c.setFont("Helvetica", 9)
     c.drawString(210, y, "Confirmado por:")
     c.setFont("Helvetica-Bold",9)
-    c.drawString(295, y, reserva.confirmada_por if reserva.confirmada_por else "")
+    c.drawString(265, y, reserva.confirmada_por if reserva.confirmada_por else "")
     
     c.setFont("Helvetica", 9)
     c.drawString(420, y, "Data da Reserva:")
     c.setFont("Helvetica-Bold", 9)
     data_formatada = reserva.data_reserva.strftime("%d/%m/%Y")
-    c.drawString(510, y, data_formatada)
+    c.drawString(480, y, data_formatada)
 
     
     
